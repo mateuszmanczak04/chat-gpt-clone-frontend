f42b811263a869172671ff30de4bae584d9b7750
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-08-16 14:47:21 +0200

    graphic interface

---
+++ src/app/chats/chats.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats',
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.scss'],
+})
+export class ChatsComponent {
+  chats = [
+    {
+      title: 'Chat 1',
+    },
+    {
+      title: 'Chat 2',
+    },
+    {
+      title: 'Chat 3',
+    },
+    {
+      title: 'Chat 4',
+    },
+    {
+      title: 'Chat 5',
+    },
+    {
+      title: 'Chat 6',
+    },
+  ];
+}
